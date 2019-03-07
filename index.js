e1967c2084b0627c7d7939a6ee35244dc6b94e1d
Maike Mota <maike_henrique@hotmail.com>
2019-03-07 12:22:15 -0300

    Added Twilio integration

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const Nightmare = require('nightmare')
 const nightmare = Nightmare()
 const opn = require('opn');
+
+const client = require('twilio')(process.env.TWILIO_ACC_ID, process.env.TWILIO_AUTH_TOKEN);
 
 const BASE_URL = `http://www.guardamirimlondrina.org.br/editais-de-contratacao-2019/`;
 const LAST_PDF = 'http://www.guardamirimlondrina.org.br/Agendamento_Entrevistas_AssProjetos00619.pdf';
@@ -33,13 +37,34 @@ async function main() {
         if (result.changed) {
             await opn(result.url)
             console.info(`${new Date().toISOString()} - Opening new content and exiting!`)
-            process.exit(0);
+            let sendTo = process.env.SEND_TO.split(',');
+            for (let to of sendTo) {
+                await sendMessage(to, result.url);
+            }
+            clearInterval(interval);
+            process.exit(0)
+
         }
     } catch (e) {
         console.error(`${new Date().toISOString()} - ${e}`);
     }
 }
 
+async function sendMessage(to, url) {
+    return new Promise(async resolve => {
+        await client.messages
+            .create({
+                body: 'Resultado saiu! Veja: ' + url,
+                from: 'whatsapp:+14155238886',
+                to: `${to}`
+            })
+            .then((mesage) => {
+                resolve()
+                console.info(`${new Date().toISOString()} - Message sent to ${to}!`);
+            });
+    })
+}
+
+var interval = setInterval(async () => await main(), parseInt(process.env.POLLING_INTERVAL_MIN) * 1000 * 60)
 main();
-setInterval(async () => await main(), 5 * 1000 * 60)
 
